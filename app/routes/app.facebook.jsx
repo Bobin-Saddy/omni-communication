eed32bbe8bf842312894e7f50a16a359a60ad80f
Digittrix Dev <dev@digittrix.in>
2025-07-17 18:16:35 +0530

    cdcdc

--- app/routes/app.facebook.jsx
+++ app/routes/app.facebook.jsx
@@ -149,34 +149,45 @@ export default function FacebookPagesConversations() {
   };
 
   // Polling for new messages in all conversations
-  useEffect(() => {
-    const interval = setInterval(() => {
-      if (selectedPage && conversations.length > 0) {
-        conversations.forEach((conv) => {
-          const accessToken = pageAccessTokens[selectedPage.id];
-          fetch(
-            `https://graph.facebook.com/${conv.id}/messages?fields=message,from,created_time&access_token=${accessToken}`
-          )
-            .then((res) => res.json())
-            .then((data) => {
-              if (data.data && data.data.length > 0) {
-                const lastMsg = data.data[0];
-                const isOwn = lastMsg.from?.name === selectedPage.name;
-                if (!isOwn) {
-                  // New message from user
+// Polling for new messages in all conversations
+useEffect(() => {
+  const interval = setInterval(() => {
+    if (selectedPage && conversations.length > 0) {
+      conversations.forEach((conv) => {
+        const accessToken = pageAccessTokens[selectedPage.id];
+        fetch(
+          `https://graph.facebook.com/${conv.id}/messages?fields=message,from,created_time&access_token=${accessToken}`
+        )
+          .then((res) => res.json())
+          .then((data) => {
+            if (data.data && data.data.length > 0) {
+              const lastMsg = data.data[0];
+              const isOwn = lastMsg.from?.name === selectedPage.name;
+
+              if (!isOwn) {
+                if (
+                  selectedConversation &&
+                  selectedConversation.id === conv.id
+                ) {
+                  // If in current open conversation, update messages directly
+                  fetchMessages(conv);
+                } else {
+                  // If not in current conversation, show new message popup
                   setNewMessages((prev) => ({ ...prev, [conv.id]: true }));
                 }
               }
-            })
-            .catch((err) =>
-              console.error("Error polling conversation messages:", err)
-            );
-        });
-      }
-    }, 2000); // polling every 2 seconds
+            }
+          })
+          .catch((err) =>
+            console.error("Error polling conversation messages:", err)
+          );
+      });
+    }
+  }, 2000); // polling every 2 seconds
 
-    return () => clearInterval(interval);
-  }, [selectedPage, conversations]);
+  return () => clearInterval(interval);
+}, [selectedPage, conversations, selectedConversation]);
+
 
   // Styles
   const styles = {
