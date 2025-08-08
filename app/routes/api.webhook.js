f7275e8ebc1f9963dcfa321ef84e86930563ff41
Digittrix Dev <dev@digittrix.in>
2025-08-08 18:18:03 +0530

    check-update

--- app/routes/api.webhook.js
+++ app/routes/api.webhook.js
@@ -32,7 +32,7 @@ export async function loader({ request }) {
   const token = url.searchParams.get("hub.verify_token");
   const challenge = url.searchParams.get("hub.challenge");
 
-  if (mode && token && mode === "subscribe" && token === "your_verify_token") {
+  if (mode && token && mode === "subscribe" && token === "EAAHvZAZB8ZCmugBPFdZCUlEs44dgjYSzHMDhQu5JT14ZAtQyB06gJ5TFJNc14Pinhlxy9e7fuw89eOnZC9noLqoRYGaU3cI8q9NTOgZB99YnZAYJvp8oCejlgP0SOZC5ZBgFoLIk8yACqR2DV9zFtumaPwfwO92ZAypaU2Nnu6zo2jBHFdfvmZCeWieOZBROaWUxuJmxtIBscdxaSI9I0OnQhsFJ9E83i8kfOYsg5ZAoTD3GONtwZDZD") {
     return new Response(challenge, { status: 200 });
   } else {
     return new Response("Forbidden", { status: 403 });
